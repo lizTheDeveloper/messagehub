2bef4ca35c2f39fc0e28e1690a02bbfcca8c4086
jvfoxiv <jvfoxiv@gmail.com>
2015-03-09 13:21:47 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,7 +2,6 @@ CREATE TABLE messages (
   type_token varchar(100) NOT NULL,
   channel_token varchar(100) NOT NULL,
   user_name varchar(100) NOT NULL,
-  user_ip varchar(100) NOT NULL,
   message_text text NOT NULL,
   message_timestamp timestamptz DEFAULT localtimestamp NOT NULL
 );
