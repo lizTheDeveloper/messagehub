4cd77b497f888952d7a9b319374f03538b4fea91
jvfoxiv <jvfoxiv@gmail.com>
2015-03-10 09:53:47 -0700

    add ip

--- schema.sql
+++ schema.sql
@@ -1,7 +1,11 @@
+CREATE DATABASE messagehub;
+\c messagehub;
+
 CREATE TABLE messages (
   type_token varchar(100) NOT NULL,
   channel_token varchar(100) NOT NULL,
   user_name varchar(100) NOT NULL,
+  user_ip varchar(100) NOT NULL,
   message_text text NOT NULL,
   message_timestamp timestamptz DEFAULT localtimestamp NOT NULL
 );
